01b748564cef1abf53997d88783d9be0f6af2075
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-19 02:27:38 -0400

    Export the App component using hot module to see the updated chages without refreshing the browser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 import './App.css';
 
 const App = () => {
   return (
     <div className='container'>
-      SmartTodo
+      SmartTodo !!!!!!
     </div>
   );
 };
 
-export default App;
+export default hot(module)(App);

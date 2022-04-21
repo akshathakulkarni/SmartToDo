a46f3567dcadd58ffc1103d5052cb2eb0bc2eb01
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-21 15:22:12 -0400

    Create a constant configureStore that creates the redux store while persisting data and export it

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
@@ -8,15 +8,17 @@ const reducers = {
   todos,
 };
 
+//persistConfig is an object that tells Redux Persist how to save and where to store data.
 const persistConfig = {
   key: 'root',
-  storage,
+  storage, //defaults to localStorage on web
   stateReconciler: autoMergeLevel2, 
 }
-//autoMergeLevel2 tells redux-persist how to handle the
-//initial and stored states of application.
+//autoMergeLevel2 tells redux-persist how to handle the initial and stored states of application.
 const rootReducer = combineReducers(reducers);
 //persistConfig is an object that tells Redux how to save and store apps data.
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = createStore(persistedReducer);
+const configureStore = createStore(persistedReducer);
+
+export default configureStore;
